7eefcb4d5567ccde774248f7e26872421f3c11fa
rofeldsp <igordmkr@gmail.com>
2019-12-26 19:16:36 +0300

    start

---
+++ cmake-build-debug/CMakeFiles/printf_owngit.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/printf_owngit.dir/ft_printf.c.o"
+  "printf_owngit"
+  "printf_owngit.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/printf_owngit.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ list.h
@@ -0,0 +1,12 @@
+//
+// Created by Rosanne Feldspar on 26/12/2019.
+//
+
+#ifndef LIST_H
+#define LIST_H
+
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdarg.h>
+
+#endif
