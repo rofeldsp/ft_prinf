3b366c9806450bc03c5f81b620caae91c6ad4d7d
rofeldsp <igordmkr@gmail.com>
2020-02-15 22:05:58 +0300

    67 norme floats

--- parse_float.c
+++ parse_float.c
@@ -12,7 +12,7 @@
 
 #include "ft_printf.h"
 
-void 		parse_float8(t_print *node, __int64_t *num, char **str)
+void		parse_float8(t_print *node, __int64_t *num, char **str)
 {
 	if (node->flag & ZERO && !(node->flag & MINUS))
 	{
@@ -41,10 +41,12 @@ void		parse_float7(t_print *node, __int64_t *num, char **str)
 				node->buffer[node->field_start] = '+';
 		}
 		else
-			node->buffer[node->pointer > node->field_start ? node->pointer - 1 : node->pointer++] = '+';
+			node->buffer[node->pointer > node->field_start ? node->pointer - 1
+												: node->pointer++] = '+';
 	}
 	if (node->flag & MINUS)
-		node->pointer = node->field_start + node->flag & PLUS && *num >= 0 ? 1 : 0;
+		node->pointer = node->field_start + node->flag & PLUS
+											&& *num >= 0 ? 1 : 0;
 }
 
 void		parse_float6(t_print *node, __int64_t *num)
@@ -105,10 +107,10 @@ void		parse_float4(t_print *node, char **tmp, char **str, char **str2)
 char		*float_cut(__int64_t *num, t_print *node)
 {
 	char	*tmp;
-	int 	a;
+	int		a;
 	int		sign;
-	char 	*str2;
-	char 	*str;
+	char	*str2;
+	char	*str;
 
 	a = 0;
 	sign = node->fnumber < 0 ? -1 : 1;
@@ -117,19 +119,17 @@ char		*float_cut(__int64_t *num, t_print *node)
 	parse_float3(num, &str2, &a);
 	if (*num == 0 && sign == -1)
 	{
-		if (!(str = ft_strjoin("-", tmp = ft_itoa(*num))))
+		tmp = ft_itoa(*num);
+		if (!(str = ft_strjoin("-", tmp)))
 			exit(-1);
 		free(tmp);
 	}
 	else if (!(str = ft_itoa(*num)))
-			exit(-1);
+		exit(-1);
 	parse_float4(node, &tmp, &str, &str2);
 	*node = adjust_to_width(*node, (ft_strlen(str)));
 	*node = adjust_to_flag3(*node, (ft_strlen(str)), str);
 	parse_float5(node, num, &str);
-	parse_float6(node, num);
-	parse_float7(node, num, &str);
-	parse_float8(node, num, &str);
 	return (str);
 }
 
@@ -162,13 +162,13 @@ void		parse_float3(__int64_t *num, char **str2, int *a)
 	(*str2)[b] = '\0';
 }
 
-char 		*parse_float2(t_print *node, int *a)
+char		*parse_float2(t_print *node, int *a)
 {
-	int pres;
-	int last_char;
-	uint64_t floatnbr;
-	int pres2;
-	char *str2;
+	int			pres;
+	int			last_char;
+	uint64_t	floatnbr;
+	int			pres2;
+	char		*str2;
 
 	pres = node->precision != -1 ? node->precision : 0;
 	str2 = ft_strnew(pres + 1);
@@ -180,14 +180,11 @@ char 		*parse_float2(t_print *node, int *a)
 			pres2 = pres;
 		floatnbr = node->fnumber * ft_power(10, pres2 + 1);
 		node->fnumber *= ft_power(10, pres2 + 1);
-		node->fnumber -= (uint64_t )node->fnumber;
+		node->fnumber -= (uint64_t)node->fnumber;
 		*a += pres2 + 1;
 		last_char = *a - 1;
 		while (pres2-- >= 0)
-		{
-			str2[last_char--] = floatnbr % 10 + '0';
-			floatnbr /= 10;
-		}
+			str2[last_char--] = (floatnbr /= 10) % 10 + '0';
 		pres = pres > 18 ? (pres - 18 - 1) : pres2;
 	}
 	return (str2);
@@ -195,29 +192,18 @@ char 		*parse_float2(t_print *node, int *a)
 
 t_print		parse_float(t_print node)
 {
-	char *str;
-	int i;
-	__int64_t num;
-
+	char		*str;
+	int			i;
+	__int64_t	num;
 
 	num = (__int64_t)node.fnumber;
 	node.fnumber -= num;
 	node.precision = node.precision == -2 ? 6 : node.precision;
 	node.field_start = node.pointer;
 	str = float_cut(&num, &node);
-//	if (node.flag & ZERO && !(node.flag & MINUS))
-//	{
-//		if (num < 0 || node.flag & PLUS)
-//		{
-//			node.buffer[node.field_start] = num < 0 ? '-' : '+';
-//			str = ft_strcpy(str, &(str[num < 0 ? 1 : 0]));
-//			node.empty_space -= num < 0 ? 0 : 1;
-//			node.pointer += num < 0 ? 1 : 0;
-//			node.field_start++;
-//		}
-//		while (node.field_start < node.pointer)
-//			node.buffer[node.field_start++] = '0';
-//	}
+	parse_float6(&node, &num);
+	parse_float7(&node, &num, &str);
+	parse_float8(&node, &num, &str);
 	i = 0;
 	while (str[i])
 	{
@@ -231,8 +217,8 @@ t_print		parse_float(t_print node)
 
 __uint64_t	ft_power(int base, int power)
 {
-	__uint64_t res;
-	int i;
+	__uint64_t	res;
+	int			i;
 
 	i = 1;
 	res = base;
