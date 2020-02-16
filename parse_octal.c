d63643db70e032029da754ca481194a4e6bb690c
Igor Krasnov <igordmkr@gmail.com>
2020-02-16 18:34:09 +0300

    68 - norminating

--- parse_octal.c
+++ parse_octal.c
@@ -4,53 +4,73 @@
 
 #include "ft_printf.h"
 
+void		parse_octal3(t_print *node, char **str)
+{
+	int 	i;
+
+	i = 0;
+	if (node->flag & OCTO)
+	{
+		node->buffer[node->unumber == 0 && node->width >
+		(int)ft_strlen(*str) + 1 && !(node->flag & MINUS) ?
+						node->pointer++ + 1 : node->pointer++] = '0';
+		if ((node->precision > 0 && node->pointer == node->field_start + 1) ||
+															node->unumber == 0)
+			node->precision--;
+		if ((*str)[i] == '0')
+			(*str)[i] = '\0';
+	}
+	*node = adjust_to_precision(*node, ft_strlen(*str), str);
+	while ((*str)[i])
+	{
+		check_overflow(node);
+		node->buffer[node->pointer++] = (*str)[i++];
+	}
+	node->input++;
+	if (node->buffer[node->pointer] != '\0')
+		node->pointer++;
+}
+
+void		parse_octal2(t_print *node, char **str, char c)
+{
+	*node = adjust_to_width(*node, (node->precision == -1 ? 0 :
+	(node->flag & OCTO ? ft_strlen(*str) + 1 : ft_strlen(*str))));
+	*node = adjust_to_flag2(*node, (node->flag & OCTO ? ft_strlen(*str) + 1
+	: ft_strlen(*str)), c, *str);
+	if ((node->flag & SPACE) && (!(node->flag & PLUS)) && node->number >= 0)
+	{
+		check_overflow(node);
+		if (node->flag & ZERO)
+		{
+			node->buffer[node->pointer - node->empty_space] = ' ';
+			node->empty_space = 0;
+		}
+		else
+			node->buffer[node->pointer++] = ' ';
+	}
+	node->pointer += node->empty_space;
+	if (node->unumber == 0 && node->width > (int)ft_strlen(*str) + 1 &&
+		!(node->flag & MINUS) && (node->flag & ZERO) && (*str)[0] == '0')
+		node->buffer[node->pointer] = '0';
+}
+
 t_print 	parse_octal(t_print node, char c)
 {
 	char	*str;
-	int		i;
 
-	node.unumber = (node.size & L) ? va_arg(node.ap, unsigned long int) :
-				   ((node.size & LL) ? va_arg(node.ap, unsigned long long int) :
-					va_arg(node.ap, unsigned int));
+	if (node.size & L)
+		node.unumber = va_arg(node.ap, unsigned long int);
+	else
+		node.unumber = (node.size & LL) ? va_arg(node.ap,
+			unsigned long long int) : va_arg(node.ap, unsigned int);
 	node.field_start = node.pointer;
-	str = (node.size & H) ? ft_itoa_base((unsigned short)node.unumber, 8, c):
-		  (node.size & HH ? ft_itoa_base((unsigned char)node.unumber, 8, c):
-		   ft_itoa_base(node.unumber, 8 , 'a'));
-	i = 0;
-	node = adjust_to_width(node, (node.precision == -1 ? 0 : (node.flag & OCTO ? ft_strlen(str) + 1 : ft_strlen(str))));
-	node = adjust_to_flag2(node, (/*node.precision == -1 ? 0 : */(node.flag & OCTO ? ft_strlen(str) + 1 : ft_strlen(str))), c, str);
-	if ((node.flag & SPACE) && (!(node.flag & PLUS)) && node.number >= 0)
-	{
-		check_overflow(&node);
-		if (node.flag & ZERO)
-		{
-			node.buffer[node.pointer - node.empty_space] = ' ';
-			node.empty_space = 0;
-		}
-		else
-			node.buffer[node.pointer++] = ' ';
-	}
-	node.pointer += node.empty_space;
-	if (node.unumber == 0 && node.width > (int)ft_strlen(str) + 1 && !(node.flag & MINUS) && (node.flag & ZERO) && str[i] == '0')
-		node.buffer[node.pointer] = '0';
-	if (node.flag & OCTO)
-	{
-		node.buffer[node.unumber == 0 && node.width > (int)ft_strlen(str) + 1 && !(node.flag & MINUS) ? node.pointer++ + 1 : node.pointer++] = '0';
-		if ((node.precision > 0 && node.pointer == node.field_start + 1) || node.unumber == 0)
-			node.precision--;
-		if (str[i] == '0')
-			str[i] = '\0';
-	}
-	node = adjust_to_precision(node, ft_strlen(str), &str);
-	while (str[i])
-	{
-		check_overflow(&node);
-		node.buffer[node.pointer++] = str[i];
-		i++;
-	}
-	node.input++;
-	if (node.buffer[node.pointer] != '\0')
-		node.pointer++;
+	if (node.size & H)
+		str = ft_itoa_base((unsigned short)node.unumber, 8, c);
+	else
+		str = (node.size & HH ? ft_itoa_base((unsigned char)node.unumber, 8, c)
+						: ft_itoa_base(node.unumber, 8 , 'a'));
+	parse_octal2(&node, &str, c);
+	parse_octal3(&node, &str);
 	free(str);
 	return (node);
 }
